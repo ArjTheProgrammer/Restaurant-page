2d32fbca10b61f4606a122125301edfdea9f697c
Jose Neil Silagan <neilsilagan@gmail.com>
2024-07-15 13:34:32 +0800

    Add style to the body and header

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
@@ -17,4 +16,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
 };
